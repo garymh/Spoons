0bf4c0aae98f01cc2885ec243021d62d923df282
Gary Holtz <code@garyh.org>
2020-12-26 14:32:12 -0600

    another bug fix

--- Source/MicMute.spoon/init.lua
+++ Source/MicMute.spoon/init.lua
@@ -1,26 +1,23 @@
 --- === MicMute ===
---- Customized by Gary Holtz
+--- Fork by Gary Holtz (@garymh)
 ---
 --- Microphone Mute Toggle and status indicator
 ---
 --- Download: [https://github.com/dctucker/Spoons/raw/master/Spoons/MicMute.spoon.zip](https://github.com/dctucker/Spoons/raw/master/Spoons/MicMute.spoon.zip)
-
-local obj={}
+local obj = {}
 obj.__index = obj
 
 -- Metadata
-obj.name = "MicMute*"
+obj.name = "MicMute"
 obj.version = "1.0"
-obj.author = "dctucker <dctucker@github.com> (modified by Gary Holtz)"
+obj.author = "dctucker <dctucker@github.com>"
 obj.homepage = "https://dctucker.com"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
 function obj:updateMicMute(muted)
-  if muted == -1 then
-    muted = hs.audiodevice.defaultInputDevice():muted()
-  end
+  if muted == -1 then muted = hs.audiodevice.defaultInputDevice():muted() end
   if muted then
-    obj.mute_menu:setTitle("📵 Muted")
+    obj.mute_menu:setTitle("📵Muted")
   else
     obj.mute_menu:setTitle("")
   end
@@ -32,35 +29,27 @@ end
 ---
 function obj:toggleMicMute()
   local mic = hs.audiodevice.defaultInputDevice()
-  local zoom = hs.application'Zoom'
-  local chrome = hs.application'Chrome'
+  local zoom = hs.application 'Zoom'
   if mic:muted() then
     mic:setMuted(false)
     if zoom then
-      local ok = zoom:selectMenuItem'Unmute Audio'
+      local ok = zoom:selectMenuItem 'Unmute Audio'
       if not ok then
         hs.timer.doAfter(0.5, function()
-          zoom:selectMenuItem'Unmute Audio'
+          zoom:selectMenuItem 'Unmute Audio'
         end)
       end
-    elseif chrome and string.match(chrome:mainWindow():title(), "Meet - ") then
-      obj:muteMeet()
     end
   else
     mic:setMuted(true)
     if zoom then
-      local ok = zoom:selectMenuItem'Mute Audio'
+      local ok = zoom:selectMenuItem 'Mute Audio'
       if not ok then
-        hs.timer.doAfter(0.5, function()
-          zoom:selectMenuItem'Mute Audio'
-        end)
+        hs.timer.doAfter(0.5, function() zoom:selectMenuItem 'Mute Audio' end)
       end
-    elseif chrome and string.match(chrome:mainWindow():title(), "Meet - ") then
-      obj:muteMeet()
     end
-
   end
-  updateMicMute(-1)
+  obj:updateMicMute(-1)
 end
 
 --- MicMute:bindHotkeys(mapping, latch_timeout)
@@ -72,48 +61,36 @@ end
 ---   * toggle - This will cause the microphone mute status to be toggled. Hold for momentary, press quickly for toggle.
 ---  * latch_timeout - Time in seconds to hold the hotkey before momentary mode takes over, in which the mute will be toggled again when hotkey is released. Latch if released before this time. 0.75 for 750 milliseconds is a good value.
 function obj:bindHotkeys(mapping, latch_timeout)
-	if (self.hotkey) then
-		self.hotkey:delete()
-	end
-	local mods = mapping["toggle"][1]
-	local key = mapping["toggle"][2]
+  if (self.hotkey) then self.hotkey:delete() end
+  local mods = mapping["toggle"][1]
+  local key = mapping["toggle"][2]
 
-	if latch_timeout then
-		self.hotkey = hs.hotkey.bind(mods, key, function()
-			self:toggleMicMute()
-			self.time_since_mute = hs.timer.secondsSinceEpoch()
-		end, function()
-			if hs.timer.secondsSinceEpoch() > self.time_since_mute + latch_timeout then
-				self:toggleMicMute()
-			end
-		end)
-	else
-		self.hotkey = hs.hotkey.bind(mods, key, function()
-			self:toggleMicMute()
-		end)
-	end
+  if latch_timeout then
+    self.hotkey = hs.hotkey.bind(mods, key, function()
+      self:toggleMicMute()
+      self.time_since_mute = hs.timer.secondsSinceEpoch()
+    end, function()
+      if hs.timer.secondsSinceEpoch() > self.time_since_mute + latch_timeout then
+        self:toggleMicMute()
+      end
+    end)
+  else
+    self.hotkey = hs.hotkey.bind(mods, key, function() self:toggleMicMute() end)
+  end
 
-	return self
-end
-
-function obj:muteMeet()
-  hs.osascript.applescriptFromFile("/Users/gary/code/dotfiles/home/hammerspoon/MuteMeet.scpt")
+  return self
 end
 
 function obj:init()
-	obj.time_since_mute = 0
-	obj.mute_menu = hs.menubar.new()
-	obj.mute_menu:setClickCallback(function()
-		obj:toggleMicMute()
-	end)
-	obj:updateMicMute(-1)
+  obj.time_since_mute = 0
+  obj.mute_menu = hs.menubar.new()
+  obj.mute_menu:setClickCallback(function() obj:toggleMicMute() end)
+  obj:updateMicMute(-1)
 
-	hs.audiodevice.watcher.setCallback(function(arg)
-		if string.find(arg, "dIn ") then
-			obj:updateMicMute(-1)
-		end
-	end)
-	hs.audiodevice.watcher.start()
+  hs.audiodevice.watcher.setCallback(function(arg)
+    if string.find(arg, "dIn ") then obj:updateMicMute(-1) end
+  end)
+  hs.audiodevice.watcher.start()
 end
 
 return obj
